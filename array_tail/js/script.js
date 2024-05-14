3bf5bf1c361dfcf401eb48daeadacbcfc843dfa5
bigazzi96 <fbigazzi96@gmail.com>
2024-05-15 00:01:57 +0200

    Aggiunta formula per generale numeri random

---
+++ array_tail/js/script.js
@@ -0,0 +1,11 @@
+//* Array
+const array[];
+
+//*chiedo quanti elementi di devono essere
+const elementi= parseInt (prompt('quanti elementi deve contenere l'arrey?'));
+
+//*formula per genere numeri random 
+for (var i = 0; i < numElementi; i++) {
+    var  numero = Math.floor(Math.random() * 100) + 1);
+
+    
