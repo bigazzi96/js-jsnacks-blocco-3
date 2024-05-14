0850e0a0a8c074178765b9b2a520816bf2da98f3
bigazzi96 <fbigazzi96@gmail.com>
2024-05-14 23:12:12 +0200

    created scalfolding

--- array-lista/js/script.js
+++ array-lista/js/script.js
@@ -1,19 +1,1 @@
 
-//* Array con i frutti presenti
-const frutti = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola'];
-
-
-//* Aggiungi la pesca ai frutti presenti
-frutti.push('pesca');
-
-
-
-//* cerco se c'è il cocomero nella lista
-let cocomero = false;
-for (let  frutta of frutti) {
-    if (frutta === 'cocomero') {
-        cocomero= true;
-        
-    }
-
-
