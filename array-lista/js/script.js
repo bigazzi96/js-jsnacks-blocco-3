ddcdcb00b01525de5023cae36c270cea9c6ca8c9
bigazzi96 <fbigazzi96@gmail.com>
2024-05-14 23:21:42 +0200

    aggiunte condizioni per ottenere il risultato

--- array-lista/js/script.js
+++ array-lista/js/script.js
@@ -1,1 +1,25 @@
 
+//* Array con i frutti presenti
+const frutti = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola'];
+
+
+//* Aggiungi la pesca ai frutti presenti
+frutti.push('pesca');
+
+
+
+//* cerco se c'è il cocomero nella lista
+let cocomero = false;
+for (let  frutta of frutti) {
+    if (frutta === 'cocomero') {
+        cocomero= true;
+        
+    }
+}
+
+//* risulato 
+if (cocomero) {
+    console.log("Trovato! Devo solo preparare il cocktail.");
+} else {
+    console.log("Oh no, devo uscire a comprare il cocomero!");
+}
