0b802af4426a6acd98231811db067b12842733f0
bigazzi96 <fbigazzi96@gmail.com>
2024-05-14 19:40:36 +0200

    aggiunto array e funzione push

---
+++ array-lista/js/script.js
@@ -0,0 +1,7 @@
+
+//* Array con i frutti presenti
+cost frutti = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola'];
+
+
+//* Aggiungi la pesca ai frutti presenti
+frutti.push('pesca');
