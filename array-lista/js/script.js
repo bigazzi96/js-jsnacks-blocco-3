b99400c695ee74f8736865cbf69511d1fdc4bbfe
bigazzi96 <fbigazzi96@gmail.com>
2024-05-14 20:05:09 +0200

    aggiunta funzione per cercare elemento

--- array-lista/js/script.js
+++ array-lista/js/script.js
@@ -1,7 +1,19 @@
 
 //* Array con i frutti presenti
-cost frutti = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola'];
+const frutti = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola'];
 
 
 //* Aggiungi la pesca ai frutti presenti
 frutti.push('pesca');
+
+
+
+//* cerco se c'è il cocomero nella lista
+let cocomero = false;
+for (let  frutta of frutti) {
+    if (frutta === 'cocomero') {
+        cocomero= true;
+        
+    }
+
+
