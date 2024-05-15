363470de78cbf9ca299b7b6f2f444062c5a30ce1
bigazzi96 <fbigazzi96@gmail.com>
2024-05-15 12:23:32 +0200

    snack-10

---
+++ snack 10/js/script.js
@@ -0,0 +1,11 @@
+let shorarray = [1, 2, 3];
+let longarray = [4, 5, 6, 7, 8];
+
+
+if (shorarray.length < longarray.length) {
+    while (array1.length < array2.length)
+}
+
+else if (longarray.length < shorarray.length) {
+while (array2.length < array1.length) }
+                
